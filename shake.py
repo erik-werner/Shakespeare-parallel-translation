e4d6875f3a5f983d2d545f8724785bb82f81df49
Rasmus Einarsson <mr@rasmuseinarsson.se>
2018-05-30 21:35:40 +0200

    Parse out all the units

--- shake.py
+++ shake.py
@@ -2,20 +2,68 @@ import attr
 import json
 import re
 import xml.etree.ElementTree as ElementTree
-from typing import List
+from typing import Sequence, Union, Tuple
 
-@attr.s
-class Intro:
-    text : str
 
-@attr.s
-class Act:
-    text : str
+# @attr.s
+# class Intro:
+#     text : str
 
-@attr.s
-class Play:
-    intro : Intro
-    acts : List[Act]
+# @attr.s
+# class Utterance:
+#     text : str
+
+# @attr.s
+# class Person:
+#     text : str
+
+# @attr.s
+# class Text:
+#     text : str
+
+# @attr.s
+# class Footnote:
+#     text : str
+
+# @attr.s
+# class SpeechInstruction:
+#     children : Sequence[Union[Person, Text]]
+
+# @attr.s
+# class Line:
+#     children : Sequence[Union[Utterance, SpeechInstruction, Footnote]]
+
+# @attr.s
+# class Speaker:
+#     text : str
+
+# @attr.s
+# class Lines:
+#     children : Sequence[Line]
+
+# @attr.s
+# class Speech:
+#     children : Tuple[Speaker, Lines]
+
+# @attr.s
+# class Scene:
+#     units : Sequence[Union[Speech, Instruction]]
+
+# @attr.s
+# class Act:
+#     scenes : Sequence[Scene]
+
+# @attr.s
+# class Acts:
+#     children : A
+
+# @attr.s
+# class Play:
+#     parts : Tuple[Intro, Acts]
+#     intro : Intro
+#     acts : Sequence[Act]
+
+
 
 
 def apply_filters(filters, data):
@@ -32,6 +80,9 @@ def replace_tab_tags(data):
 
 def remove_page_breaks(data):
     return re.sub(r'</poem>\n+<poem>', '\n ', data)
+
+def keep_maximum_two_line_breaks(data):
+    return re.sub(r'\n\n+', '\n\n', data)
 
 def enclose_in_xml_tag(data, tag):
     return f'<{tag}>{data}</{tag}>'
@@ -66,7 +117,7 @@ def group_scenes_into_acts(data):
     data['acts'] = acts
     return data
 
-def split_scene_into_heading_and_rest(scene):
+def split_scene(scene):
     newlines = scene.text
     heading = scene[0]
 
@@ -75,18 +126,27 @@ def split_scene_into_heading_and_rest(scene):
 
     scene_text = ElementTree.tostring(element=scene, encoding='unicode')
 
+    m = re.match(
+        r'^<chapter.+?>\s*<h(1|2)>.+?</h(1|2)>\s+(?P<location>.+?)\n\n(?P<rest>.+)',
+        scene_text,
+        flags=re.DOTALL
+        )
+
+    assert m, scene_text[:100]
+
     scene = re.match(r'\n+(.+?)\n\n(.+)', scene_text, flags=re.DOTALL)
     scene = {
         'heading': heading.text,
-        'text': scene_text
+        'location': m.group('location'),
+        'units': m.group('rest').split('\n\n')
         }
 
     return scene
 
-def split_scenes_into_heading_and_rest(data):
+def split_scenes(data):
     data['acts'] = [
         {
-            'scenes': [split_scene_into_heading_and_rest(s)
+            'scenes': [split_scene(s)
             for s in act['scenes']]
         }
         for act in data['acts']
@@ -94,8 +154,23 @@ def split_scenes_into_heading_and_rest(data):
 
     return data
 
+# def split_rest_into_units(data):
+#     for act in data['acts']:
+#         for scene in act['scenes']:
+#             scene['units'] = scene['text'].split('\n\n')
+#             del scene['text']
+#     return data
+
+# def remove_scene_heading_and_find_location(scene):
+
+# def remove_scene_headings_and_find_locations(data):
+#     for act in data['acts']:
+#         for scene in act['scenes']:
+#             scene['units']
+
 
 filters = [
+    keep_maximum_two_line_breaks,
     remove_tab_char,
     replace_tab_tags,
     remove_page_breaks,
@@ -103,14 +178,24 @@ filters = [
     parse_xml,
     find_intro_and_scenes,
     group_scenes_into_acts,
-    split_scenes_into_heading_and_rest,
+    split_scenes,
 ]
+
+import itertools
+import json
 
 if __name__ == '__main__':
     with open('data/Hamlet/svensk.txt') as f:
         data = f.read()
 
     result = apply_filters(filters, data)
+
+    all_scenes = itertools.chain(*(a['scenes'] for a in result['acts']))
+    all_units = itertools.chain(*(s['units'] for s in all_scenes))
+
+    with open('units.json', 'w') as f:
+        json.dump(list(all_units), f)
+
     # print(result)
 
     # with open('out.json', 'w') as f:
